0fcf84cfa1a2f98bf6592c09c36f78d20b346344
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2024-04-19 18:28:31 -0400

    completed tictactoe

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,17 +1,31 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol, isActive, onChangeName}) {
 
-    const [ isEditing, setIsEditing ] = useState(false)
+    const [ isEditing, setIsEditing ] = useState(false);
+    const [ playerName, setPlayerName ] = useState(initialName);
 
     function handleEditClick() {
         setIsEditing(prevEdit => !prevEdit)
-    } 
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
+    }
+
+    function handleNameChange(e) {
+        setPlayerName(e.target.value)
+    }
+    
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" onChange={handleNameChange} value={playerName} required />;
+    }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
-                {isEditing ? <input></input> : <span className="player-name">{name}</span>}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
